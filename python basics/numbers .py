91b54b9e6c65e346bf09b17682486b69aadbbbed
wanjey-coder <wanjeygikenye@gmail.com>
2026-02-11 16:15:05 +0300

    how to add subtract divide and multiply numbers

---
+++ python basics/numbers .py
@@ -0,0 +1,18 @@
+# Name: Wanjey Gikenye
+#Date : 11/02/2026
+
+#program to add, subtract, divide, multiply
+
+first_number = int(input("Enter first number"))
+second_number = int(input("Enter second number"))
+
+
+sum_numbers = first_number + second_number
+difference_numbers = first_number - second_number
+quotient_numbers = first_number / second_number
+product_numbers = first_number * second_number
+
+print(f"the sum of the numbers : {sum_numbers}")
+print(f"the quotient of the numbers : {quotient_numbers}")
+print(f"the difference of the numbers : {difference_numbers}")
+print(f"the product of the numbers : {product_numbers}")
---
+++ python basics/user_input .py
@@ -0,0 +1,8 @@
+# Name: Wanjey Gikenye
+#Date : 11/02/2026
+
+#program to get user input
+
+Name = input("What is your name")
+
+print (name)
