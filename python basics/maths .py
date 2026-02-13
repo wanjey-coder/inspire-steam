e1af366f5b3b2d3dff9ef1ab191989bc0e18a993
wanjey-coder <wanjeygikenye@gmail.com>
2026-02-13 09:34:03 +0300

    day  5

---
+++ python basics/maths .py
@@ -0,0 +1,26 @@
+#Name : Wanjey Gikenye
+#Date : 13/02/2026
+#program to do mathematical operations
+number = 16.79
+
+import math
+print(abs(number))
+
+
+angle_radians = 60
+angle_degrees = angle_radians / 180
+
+x=1
+y= math.degrees(x)
+print(math.cos(angle_radians))
+print(math.sin(angle_radians))
+print(math.tan(angle_radians))
+
+
+
+print(min(3,4))
+print(max(13,46))
+
+print(math.sqrt(144))
+
+print(25**2)
