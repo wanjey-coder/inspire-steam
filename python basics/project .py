539b45ea02c42dfceccba71618049330447ab2c6
wanjey-coder <wanjeygikenye@gmail.com>
2026-02-18 20:33:56 +0300

    Project to show personal info

---
+++ python basics/project .py
@@ -0,0 +1,11 @@
+#Name : Wanjey Gikenye
+#Date : 18/02/2026
+#program to show my personal information
+
+fav_hobby = "Playing Football"
+fav_sport = "Football"
+fav_food = "lasagna"
+
+print(fav_food)
+print(fav_sport)
+print(fav_hobby)
