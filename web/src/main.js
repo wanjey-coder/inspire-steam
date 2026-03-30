e2a738639148a911b9af2c512beff5a908d7b0e1
wanjey-coder <wanjeygikenye@gmail.com>
2026-03-30 09:10:01 +0300

    msg

---
+++ web/src/main.js
@@ -0,0 +1,18 @@
+function message(){
+alert("Hello from Wanjey");
+
+}
+
+var fnumber,snumber,sum;
+function add(fnumber,snumber)
+{
+    sumnumber = fnumber + snumber;
+    return sumnumber;
+}
+function changetext()[
+   var btn = document.getElementById("myBtn")
+   var span = document.getElementById("output")
+   var textbox = document.getElementById("textbox")
+
+   textbox.style.color = "Green"
+]
